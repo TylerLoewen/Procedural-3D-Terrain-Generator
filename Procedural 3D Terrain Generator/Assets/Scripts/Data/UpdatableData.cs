91fbddf936055df6dc9a5f530a5fcecbfac580ed
Tyler Loewen <tylerscottloewen@gmail.com>
2019-06-20 21:43:54 -0500

    Remove license from source files

--- Procedural 3D Terrain Generator/Assets/Scripts/Data/UpdatableData.cs
+++ Procedural 3D Terrain Generator/Assets/Scripts/Data/UpdatableData.cs
@@ -1,22 +1,3 @@
-/*
-<Procedural 3D Terrain Generator is a seamless chunk based terrain generator with customizable terrain types>
-Copyright (C) <2018>  <Tyler Loewen>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/gpl.txt>.
-
-Contact at tylerscottloewen@gmail.com
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
